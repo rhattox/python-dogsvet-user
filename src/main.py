f24135b2745a1446160de8ba69cb801f83d5f1aa
Bruno Souto <bcovies@gmail.com>
2023-12-21 15:55:24 -0300

    Add migration on start up (#14)

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
-from flask import Flask
 import logging
+from flask import Flask
 from .route.user import user
 from .etc.load_env import load_database_environment_variables
+from alembic.config import Config
+from alembic import command
 app = Flask(__name__)
-
-# Enable Migrations
 
 # Enable Logging
 app.logger.setLevel(logging.DEBUG)
 load_database_environment_variables()
 app.register_blueprint(user)
 
+# Enable Migrations
+alembic_cfg = Config("alembic.ini")
+command.upgrade(alembic_cfg, "head")
 
 if __name__ == "__main__":
     app.run(debug=True)
